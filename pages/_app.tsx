f932cc016ae0576f228d846501ad2c4acaac4258
Ayush Soni <ayushsoni1010.work@gmail.com>
2022-12-05 20:03:29 +0530

    update: packages and configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../common/utils/theme";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
