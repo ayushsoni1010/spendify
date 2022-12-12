152810984a46d48ca9b57554601b252dc36c17f0
Ayush Soni <ayushsoni1010.work@gmail.com>
2022-12-12 12:52:57 +0530

    chore(config): extended breakpoints

--- common/utils/theme.ts
+++ common/utils/theme.ts
@@ -2,14 +2,24 @@
 import { extendTheme } from "@chakra-ui/react";
 
 // 2. Extend the theme to include custom colors, fonts, etc
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
+
+const breakpoints = {
+  xs: "280px",
+  sm: "429px",
+  md: "768px",
+  lg: "960px",
+  xl: "1200px",
+  "2xl": "1536px",
 };
 
-const theme = extendTheme({ colors });
+const config = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({
+  config,
+  breakpoints,
+});
 
 export { theme };
