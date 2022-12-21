35d3bfe3c65913d7920d873cf872f5220d0ab6e5
Ayush Soni <ayushsoni1010.work@gmail.com>
2022-12-22 02:59:23 +0530

    feat: added footer links details

---
+++ website/Footer/__footerLinkDetails.ts
@@ -0,0 +1,22 @@
+export const linkDetails: Array<{ name: string; link: string }> = [
+  {
+    name: "Features",
+    link: "/features",
+  },
+  {
+    name: "Brand Assets",
+    link: "/brand-assets",
+  },
+  {
+    name: "Open Source",
+    link: "/opensource",
+  },
+  {
+    name: "Sponsor",
+    link: "/sponsor",
+  },
+  {
+    name: "About Us",
+    link: "/about",
+  },
+];
